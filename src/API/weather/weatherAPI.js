e427fc1af286d7310375c088d6cf1a98cbc66093
Lenny <theiign@gmail.com>
2020-04-19 18:27:03 +0200

    add error handling for weather api calls

--- src/API/weather/weatherAPI.js
+++ src/API/weather/weatherAPI.js
@@ -1,32 +1,42 @@
 const weatherAPI = (() => {
+  class LocationError extends Error {
+    name = "LocationError";
+  };
+
   const requestWeatherData = (location) => {
     return fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location}&appid=96b1e2bf4e1fdbd8e2bb97f9be4991e4`, {mode: 'cors'})
     .then(response => {
       return response.json();
     })
     .then(weatherData => {
+      if(weatherData.message === "city not found") throw new LocationError(weatherData.message);
       return weatherData;
     })
-    .catch(error => error);
+    .catch(error => {
+      if(error instanceof LocationError) {
+        alert("Location not found");
+      } else {
+        alert("Something went wrong, please try again later");
+      }
+    });
   }
 
   // extract only the relevant data
   const getWeatherData = async (location) =>{
-    const data = await requestWeatherData(location);
-
-    if(data.cod === "404") {
-      if(data.message === "city not found") alert("Location not found");
-      return null;
+    try {
+      const data = await requestWeatherData(location);
+  
+      const relevantData = {
+        description: data.weather[0].description,
+        temperature: data.main.temp,
+        temperatureFeel: data.main.feels_like,
+        name: data.name
+      };
+      
+      return relevantData;
+    } catch {
+      return {};
     }
-
-    const relevantData = {
-      description: data.weather[0].description,
-      temperature: data.main.temp,
-      temperatureFeel: data.main.feels_like,
-      name: data.name
-    };
-    
-    return relevantData;
   };
 
   return {
