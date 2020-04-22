83c530fbaadef77c92d20675bda862ff066f3b83
Lenny <theiign@gmail.com>
2020-04-22 18:01:20 +0200

    Updates

---
+++ precache-manifest.7c527e5385db46afecc97d452d3a332e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e91f6a870d7d46fb590ed8b60d349cd2",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "be2af06f831a90e0b838",
+    "url": "/weather-app/static/css/main.48d8101d.chunk.css"
+  },
+  {
+    "revision": "219256683d8b23c784c5",
+    "url": "/weather-app/static/js/2.24c026d8.chunk.js"
+  },
+  {
+    "revision": "cc72a2e36877fe2f4367ef32fa183fda",
+    "url": "/weather-app/static/js/2.24c026d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be2af06f831a90e0b838",
+    "url": "/weather-app/static/js/main.ec1c2cef.chunk.js"
+  },
+  {
+    "revision": "8ad2e2c3d0b0209b3902",
+    "url": "/weather-app/static/js/runtime-main.4479c0a4.js"
+  }
+]);
